99d1317470ba6a1f664d8fe05118898014fb9f5e
Charley Picker <sirchuckalot@users.noreply.github.com>
2025-07-05 08:20:54 -0700

    Add README and placeholder Verilog to enable public indexing

---
+++ rtl/cpu/cpu_top.v
@@ -0,0 +1,10 @@
+module (
+  // We can optionally declare parameters here
+  parameter <parameter_name> = <default_value>
+)
+cpu_top (
+  // All IO are defined here
+  <direction> <data_type> <size> <port_name>
+);
+ // Functional RTL (or structural) code
+endmodule
